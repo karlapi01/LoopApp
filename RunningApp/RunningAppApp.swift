ccd2f613b6c0e90c37e8dcae0c4653e218fd9cc1
Karla <karla.pisonic@icloud.com>
2026-05-22 12:47:44 +0200

    Initial Commit

---
+++ RunningApp/RunningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunningAppApp.swift
+//  RunningApp
+//
+//  Created by Karla Pisonic on 22.05.2026..
+//
+
+import SwiftUI
+
+@main
+struct RunningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RunningAppTests/RunningAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  RunningAppTests.swift
+//  RunningAppTests
+//
+//  Created by Karla Pisonic on 22.05.2026..
+//
+
+import Testing
+@testable import RunningApp
+
+struct RunningAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
